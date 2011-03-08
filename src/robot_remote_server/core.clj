99eff21f63413fce8726fcbc543f1097298e2915
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-03-08 14:20:07 -0500

    Make macro for handler creation, need *ns* evaluated later

--- src/robot_remote_server/core.clj
+++ src/robot_remote_server/core.clj
@@ -2,72 +2,67 @@
   (:require [necessary-evil.core :as xml-rpc]
             [clojure.string :as str])
   (:import org.mortbay.jetty.Server)
-  (:use robot-remote-server.keyword
+  (:use [robot-remote-server keyword]
         ring.adapter.jetty))
 
 (def *result* (atom {:status "PASS", :return "", :output "", :error "", :traceback ""}))
 (def *server* (atom nil))
-(def rf-ns (atom 'robot-remote-server.keyword))
-
-;; WARNING: Less-than-functional code follows
+(def server-fns ["get-keyword-names" "app-handler" "server-start!" "server-stop!" "run-keyword" "get-keyword-arguments" "get-keyword-documentation" "find-kw-fn" "clojurify-name"])
 
 (defn find-kw-fn
-  [ns-name fn-name]
-  (ns-resolve (find-ns ns-name) (symbol fn-name)))
+  [this-ns fn-name]
+  (ns-resolve this-ns (symbol fn-name)))
 
 (defn clojurify-name
   "Make it nicer for Clojure developers to write keywords; replace underscores with dashes"
   [s]
   (str/replace s "_" "-"))
 
-(defn- run-keyword
-  "Run a single keyword"
-  [kw-name args]
-  (let [clj-kw-name (clojurify-name kw-name)
-        a-fn (find-kw-fn @rf-ns clj-kw-name)
-        output (with-out-str (try
-                               (apply a-fn args)
-                               (catch Exception e
-                                 (do
-                                   (reset! *result* {:status "FAIL", :return "", :output "",
-                                                     :error (with-out-str (prn e)), :traceback (with-out-str (.printStackTrace e))})
-                                   @*result*))))]
-    (swap! *result* assoc :output output :return output)
-    @*result*))
+;; WARNING: Less-than-functional code follows
 
-(defn- get-keyword-names
-  "Get all keywords defined in rf-ns namespace, and make the names RobotFramework-friendly"
+(defmacro init-handler
+  "Create handler for XML-RPC server. Justification: delayed evaluation of *ns*"
   []
-  (vec (map #(str/replace % "-" "_") (map str (map first (ns-publics @rf-ns))))))
-
-(defn- get-keyword-arguments
-  [kw-name]
-  (let [clj-kw-name (clojurify-name kw-name)
-        a-fn (find-kw-fn @rf-ns clj-kw-name)]
-    (vec (map str (last (:arglists (meta a-fn)))))))
-
-(defn- get-keyword-documentation
-  [kw-name]
-  (let [clj-kw-name (clojurify-name kw-name)
-        a-fn (find-kw-fn @rf-ns clj-kw-name)]
-    (:doc (meta a-fn))))
-
-(declare app-handler)
-(defn server-start!
-  ([] (server-start! app-handler {:port 8270, :join? false}))
-  ([hndlr opts]
-     (reset! *server* (run-jetty hndlr opts))))
-
-(defn server-stop!
-  []
-  (.stop @*server*))
-
-(def app-handler (xml-rpc/end-point
-                  {:run_keyword run-keyword
-                   :get_keyword_names get-keyword-names
-                   :get_keyword_arguments get-keyword-arguments
-                   :get_keyword_documentation get-keyword-documentation
-                   :stop_remote_server server-stop!}))
+  (let [this-ns *ns*]
+    `(do
+       (def ~'app-handler* (xml-rpc/end-point
+                            {:get_keyword_names (fn []
+                                                  (vec
+                                                   (map #(str/replace % "-" "_")
+                                                        (remove #(re-find #"(\*|!)" %)
+                                                                (map str
+                                                                     (map first (ns-publics ~this-ns)))))))
+                             :run_keyword (fn
+                                            [kw-name# args#]
+                                            (let [clj-kw-name# (clojurify-name kw-name#)
+                                                  a-fn# (find-kw-fn ~this-ns clj-kw-name#)
+                                                  output# (with-out-str (try
+                                                                          (apply a-fn# args#)
+                                                                          (catch Exception e#
+                                                                            (do
+                                                                              (reset! *result* {:status "FAIL", :return "", :output "",
+                                                                                                :error (with-out-str (prn e#)), :traceback (with-out-str (.printStackTrace e#))})
+                                                                              @*result*))))]
+                                              (swap! *result* assoc :output output# :return output#)
+                                              @*result*))
+                             :get_keyword_arguments (fn
+                                                      [kw-name#]
+                                                      (let [clj-kw-name# (clojurify-name kw-name#)
+                                                            a-fn# (find-kw-fn ~this-ns clj-kw-name#)]
+                                                        (vec (map str (last (:arglists (meta a-fn#)))))))
+                             :get_keyword_documentation (fn
+                                                          [kw-name#]
+                                                          (let [clj-kw-name# (clojurify-name kw-name#)
+                                                                a-fn# (find-kw-fn ~this-ns clj-kw-name#)]
+                                                            (:doc (meta a-fn#))))
+                             :stop_remote_server (fn []
+                                                   (.stop @*server*))
+                             :hello (fn []
+                                      ["foo" "bar" "wowza_sauce"])}))
+       (defn ~'server-start!
+         ([] (~'server-start! ~'app-handler* {:port 8270, :join? false}))
+         ([hndlr# opts#]
+            (reset! *server* (run-jetty hndlr# opts#)))))))
 
 (comment
   
@@ -95,6 +90,6 @@
                                                          [(.toString k) (handle-return-val v)]))
       :else ret))
 
-  (defonce *server* (run-jetty #'handler {:port 8271 :join? false}))
-  (doto (Thread. #(run-jetty #'handler {:port 8271})) .start)
+  (defonce a-server (run-jetty #'app-handler {:port 8271 :join? false}))
+  (doto (Thread. #(run-jetty #'app-handler {:port 8271})) .start)
 )
