6083fe4c3deed95c280ab2bebb2663aeb2c871ac
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-03-08 17:16:36 -0500

    Add middleware to limit available endpoints

--- src/robot_remote_server/core.clj
+++ src/robot_remote_server/core.clj
@@ -18,44 +18,56 @@
   [s]
   (str/replace s "_" "-"))
 
+(defn wrap-rpc
+  "Ring middleware to limit server's response to the particular path that RobotFramework petitions"
+  [handler]
+  (fn [req]
+    (if (= "/RPC2" (:uri req))
+      (handler req)
+      (do
+        (throw (Exception. (str "You do not have access to the address: " (:uri req))))
+        (str "You do not have access to the address: " (:uri req))))))
+
 ;; WARNING: Less-than-functional code follows
 
 (defmacro init-handler
   "Create handler for XML-RPC server. Justification: delayed evaluation of *ns*"
   []
   (let [this-ns *ns*]
-    `(xml-rpc/end-point
-      {:get_keyword_names (fn []
-                            (vec
-                             (map #(str/replace % "-" "_")
-                                  (remove #(re-find #"(\*|!)" %)
-                                          (map str
-                                               (map first (ns-publics ~this-ns)))))))
-       :run_keyword (fn
-                      [kw-name# args#]
-                      (let [clj-kw-name# (clojurify-name kw-name#)
-                            a-fn# (find-kw-fn ~this-ns clj-kw-name#)
-                            output# (with-out-str (try
-                                                    (apply a-fn# args#)
-                                                    (catch Exception e#
-                                                      (do
-                                                        (reset! *result* {:status "FAIL", :return "", :output "",
-                                                                          :error (with-out-str (prn e#)), :traceback (with-out-str (.printStackTrace e#))})
-                                                        @*result*))))]
-                        (swap! *result* assoc :output output# :return output#)
-                        @*result*))
-       :get_keyword_arguments (fn
-                                [kw-name#]
-                                (let [clj-kw-name# (clojurify-name kw-name#)
-                                      a-fn# (find-kw-fn ~this-ns clj-kw-name#)]
-                                  (vec (map str (last (:arglists (meta a-fn#)))))))
-       :get_keyword_documentation (fn
+    `(->
+      (xml-rpc/end-point
+          {:get_keyword_names (fn []
+                                (vec
+                                 (map #(str/replace % "-" "_")
+                                      (remove #(re-find #"(\*|!)" %)
+                                              (map str
+                                                   (map first (ns-publics ~this-ns)))))))
+           :run_keyword (fn
+                          [kw-name# args#]
+                          (let [clj-kw-name# (clojurify-name kw-name#)
+                                a-fn# (find-kw-fn ~this-ns clj-kw-name#)
+                                output# (with-out-str (try
+                                                        (apply a-fn# args#)
+                                                        (catch Exception e#
+                                                          (do
+                                                            (reset! *result* {:status "FAIL", :return "", :output "",
+                                                                              :error (with-out-str (prn e#)), :traceback (with-out-str (.printStackTrace e#))})
+                                                            @*result*))))]
+                            (swap! *result* assoc :output output# :return output#)
+                            @*result*))
+           :get_keyword_arguments (fn
                                     [kw-name#]
                                     (let [clj-kw-name# (clojurify-name kw-name#)
                                           a-fn# (find-kw-fn ~this-ns clj-kw-name#)]
-                                      (:doc (meta a-fn#))))
-       :stop_remote_server (fn []
-                             (.stop @*server*))})))
+                                      (vec (map str (last (:arglists (meta a-fn#)))))))
+           :get_keyword_documentation (fn
+                                        [kw-name#]
+                                        (let [clj-kw-name# (clojurify-name kw-name#)
+                                              a-fn# (find-kw-fn ~this-ns clj-kw-name#)]
+                                          (:doc (meta a-fn#))))
+           :stop_remote_server (fn []
+                                 (.stop @*server*))})
+         wrap-rpc)))
 
 (defn server-start!
   ([hndlr] (server-start! hndlr {:port 8270, :join? false}))
