ee6dac68e538f08eb9c9fac54eb76255e9aef8b3
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-03-17 15:27:59 -0400

    Upgrade to necessary-evil 1.1.0 stable, add clj-webdriver as dev-dependency for use in RF tests, and bump our version to 0.2.0, with proper return value handling and all stable dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject robot-remote-server "0.1.0-SNAPSHOT"
+(defproject robot-remote-server "0.2.0"
   :description "Implementation of a RobotFramework remote server in Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [ring/ring-jetty-adapter "0.3.6"]
-                 [necessary-evil "1.0.0-SNAPSHOT"]]
+                 [necessary-evil "1.1.0"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
-                     [marginalia "0.5.0"]]
+                     [marginalia "0.5.0"]
+                     [clj-webdriver "0.1.0"]]
   :main robot-remote-server.keywords)
