fd754540d91258a53fe62944ffa244d62fef2901
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-03-22 13:03:01 -0400

    Use Clojure functions to check type of data structure instead of Java methods

--- src/robot_remote_server/util.clj
+++ src/robot_remote_server/util.clj
@@ -4,13 +4,11 @@
     "Convert everything to RobotFramework-acceptable types. See implementations in other languages for examples"
     [ret]
     (cond
-      (= (class ret) java.lang.String)              ret
-      (contains?
-       (supers (class ret)) java.lang.Number)       ret
-      (map? ret)                                    (into {}
-                                                          (for [[k v] ret]
-                                                            [(.toString k) (handle-return-val v)]))
-      (contains?
-       (supers (class ret)) java.util.Collection)   (map handle-return-val ret)
-      (nil? ret)                                    ""
+      (string? ret)  ret
+      (number? ret)  ret
+      (map? ret)     (into {}
+                           (for [[k v] ret]
+                             [(.toString k) (handle-return-val v)]))
+      (coll? ret)    (map handle-return-val ret)
+      (nil? ret)     ""
       :else ret))
