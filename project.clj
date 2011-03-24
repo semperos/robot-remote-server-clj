daef43aa20bbb4fd793d860b24c88e5d9d9d2b87
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-03-24 15:08:18 -0400

    Bump to version 0.2.2, handle fn's with variable arity

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject robot-remote-server "0.2.1"
+(defproject robot-remote-server "0.2.2"
   :description "Implementation of a RobotFramework remote server in Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [ring/ring-jetty-adapter "0.3.6"]
