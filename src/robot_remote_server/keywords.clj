4fc5150f60ba261a00afb819e7b64c8a977fe2f8
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-03-24 15:03:33 -0400

    Add keyword with variable arity, add to test as well

--- src/robot_remote_server/keywords.clj
+++ src/robot_remote_server/keywords.clj
@@ -1,12 +1,17 @@
 (ns robot-remote-server.keywords
-  (:use robot-remote-server.core)
+  (:use robot-remote-server.core :reload)
   (:import javax.swing.JOptionPane)
   (:gen-class :main true))
 
-(defn my-keyword
-  "Documentation for myKeyword"
-  [arg1 arg2]
-  (println (str "My first keyword! Arg1: " arg1 ", Arg2: " arg2)))
+;; (defn my-keyword
+;;   "Documentation for myKeyword"
+;;   [arg1 arg2 & others]
+;;   (println (str "My first keyword! Arg1: " arg1 ", Arg2: " arg2 ", Others: " others))
+
+(defn sample-keyword
+  "Documentation for my Sample Keyword"
+  [arg1 arg2 & other-ones]
+  (println (str "My sample keyword! Arg1: " arg1 ", and Arg2: " arg2)))
 
 (defn open-dialog
   "Open a JOptionPane, just testing things"
