446c9b792f6fa7075f23255cf156df955af629da
Alex Whitt <alex.whitt@vertiv.com>
2019-08-05 16:31:48 -0400

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject robot-remote-server "0.2.6"
+(defproject robot-remote-server "0.2.7-SNAPSHOT"
   :description "Implementation of a RobotFramework remote server in Clojure"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring/ring-jetty-adapter "0.3.6"]
-                 [necessary-evil "1.1.0"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
-                     [marginalia "0.5.0"]]
+                 [necessary-evil "2.0.2"]
+                 [commons-lang/commons-lang "2.6"]]
+  :dev-dependencies [[swank-clojure "1.4.3"]
+                     [marginalia "0.9.1"]]
   :main robot-remote-server.keywords)
